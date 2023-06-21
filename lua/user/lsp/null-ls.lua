64cb39df492da9aaad8a829bba2efd99ca032c35
kanenorman <kanenorman1996@gmail.com>
2023-06-21 10:20:48 -0500

    Updated null_ls linting settings

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -3,10 +3,11 @@ if not null_ls_status_ok then
   return
 end
 
+local builtins = null_ls.builtins
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-local formatting = null_ls.builtins.formatting
+local formatting = builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-local diagnostics = null_ls.builtins.diagnostics
+local diagnostics = builtins.diagnostics
 
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
 null_ls.setup {
@@ -18,7 +19,11 @@ null_ls.setup {
     formatting.black.with { extra_args = { "--fast" } },
     formatting.stylua,
     formatting.google_java_format,
-    diagnostics.flake8,
-    null_ls.builtins.formatting.codespell,
+    diagnostics.flake8.with { extra_args = { "--max-line-length", "88" } }, --black formatter defaults to 88
+    diagnostics.mypy,
+    diagnostics.isort,
+    diagnostics.eslint,
+    builtins.hover,
+    formatting.codespell,
   },
 }
