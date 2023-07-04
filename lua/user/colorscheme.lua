a1a8c910103ce4634d5b059abcf79c1d9e5d5ef8
kanenorman <kanenorman1996@gmail.com>
2023-07-03 23:01:17 -0500

    Updated colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,4 +1,11 @@
-local colorscheme = "darcula_dark"
+local colorscheme = "onedark"
+
+require("onedark").setup {
+  style = "warmer",
+  transparent = true,
+  trem_colors = true,
+}
+require("onedark").load()
 
 local status_ok, _ = pcall(vim.cmd.colorscheme, colorscheme)
 if not status_ok then
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -75,7 +75,7 @@ return packer.startup(function(use)
 
   -- Colorschemes
   use { "kanenorman/gruvbox-darker.nvim" }
-  use { "vermdeep/darcula_dark.nvim", requires = "rktjmp/lush.nvim" }
+  use { "navarasu/onedark.nvim" }
 
   -- cmp plugins
   use { "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" } -- The completion plugin
