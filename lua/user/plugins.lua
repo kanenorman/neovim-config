bca082279df4ea12524f149fe39c77ea9065d77f
kanenorman <kanenorman1996@gmail.com>
2023-09-18 09:54:11 -0400

    Plugin updates

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -75,6 +75,7 @@ return packer.startup(function(use)
 
   -- colorschemes
   use { "Yazeed1s/minimal.nvim" }
+  use { "vermdeep/darcula_dark.nvim", requires = "rktjmp/lush.nvim" }
 
   -- cmp plugins
   use { "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" } -- The completion plugin
